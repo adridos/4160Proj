90c7f07bcaa0191742de49a67f7739ca15f97ee7
Adri Dostine <66699146+adridos@users.noreply.github.com>
2024-03-26 16:15:42 -0400

    Update pottytracker.py
    updated with code from discord

--- pottytracker.py
+++ pottytracker.py
@@ -5,26 +5,24 @@ from gpiozero import MotionSensor
 
 
 pir = MotionSensor(4)
-server_url = 'http://35.192.215.225'  # Change the IP address and port to your GCP server's address
-
+server_url = 'http://35.192.215.225/tracker'
 while True:
-	#pir.wait_for_motion()
-	
-	print("Motion Detected: Potty Break!")
-	timestamp = datetime.now().strftime('%m-%d-%Y %H:%M:%S')
-	print(f'Motion Detected at {timestamp}')
-	
-	# Send data to the server
-	data = {'motion_detected': True, 'timestamp': timestamp}
-	try:
-		response = requests.post(server_url, json=data)
-		print('Data sent to server:', response.text)
+    pir.wait_for_motion()
+    
+    print("Motion Detected: Potty Break!")
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print(f'Motion Detected at {timestamp}')
+    
+    # Send data to the server
+    data = {'motion_detected': True, 'timestamp': timestamp}
+    try:
+        response = requests.post(server_url, json=data)
+        print('Data sent to server:', response.text)
 
-	except Exception as e:
-		print('Error sending data to server:', e)
-	
+    except Exception as e:
+        print('Error sending data to server:', e)
+    
 
 
-	time.sleep(1) #avoid mult detections in one moment
-	pir.wait_for_no_motion()
-
+    time.sleep(1) #avoid mult detections in one moment
+    pir.wait_for_no_motion()
